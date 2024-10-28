d3e6c2d7e0a802cb708c082e12174d6661f217d0
Daniel C <daniel.clot@ba.com>
2024-10-28 14:50:13 +0000

    current status, dbt run did not work yet

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,9 @@
+select ordr.id as order_id
+     , cust.id as customer_id
+     , pmt.amount
+from raw.JAFFLE_SHOP.CUSTOMERS cust
+join raw.JAFFLE_SHOP.ORDERS ordr
+on cust.ID = ordr.USER_ID
+join {{ ref('stg_stripe__payments') }} pmt
+on ordr.ID = pmt.orderid
+;
